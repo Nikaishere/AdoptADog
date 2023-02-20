d1d75547b2c69dc9b590c56d5d0bd18525d92ef4
Isabel-Razquin <isabel.razquin@gmail.com>
2023-02-20 21:19:58 +0100

    changed some styles and was trying to create modal component for a future

---
+++ client/src/components/Modal.js
@@ -0,0 +1,20 @@
+const Modal = () => {
+
+    return (
+        <div className="modal">
+            <div className="modal_content">
+                <div className="modal_header">
+                    <h4 className="modal_title">Modal title</h4>
+                </div>
+                <div className="modal_body">
+                    This is modal modal_content
+                </div>
+                <div className="modal_footer">
+                    <button className="button">Close</button>   
+                </div>
+            </div>
+        </div>   
+        ) 
+}
+
+export default Modal;
--- client/src/components/WhyAdopt.js
+++ client/src/components/WhyAdopt.js
@@ -2,12 +2,12 @@
 const WhyAdopt = () => {
 
     return (
-        <div classname="container" class="container text-center p-16 bg-teal-300">
+        <div classname="container" class="container text-center p-16 bg-violet-200">
             <h1 class="font-bold text-5xl p-4 text-neutral-900">Why adoption?</h1>
-            <p class="text-neutral-900">You are fighting against cruel breeding</p>
-            <p class="text-neutral-900">You are giving a second chance to a deserving dog</p>
-            <p class="text-neutral-900">Pets are good for your health</p>
-            <p class="text-neutral-900">You are helping to build a humane society</p>
+            <p class="text-neutral-900">You are fighting against cruel breeding.</p>
+            <p class="text-neutral-900">You are giving a second chance to a deserving dog.</p>
+            <p class="text-neutral-900">Pets are good for your health.</p>
+            <p class="text-neutral-900">You are helping to build a humane society.</p>
 
            <div class="p-6">
             <a href="/breeds"><button class="bg-transparent hover:bg-neutral-900 text-neutral-900 font-semibold hover:text-yellow-400 py-2 px-4 border border-neutral-900 hover:border-transparent rounded">Find your match</button></a>
