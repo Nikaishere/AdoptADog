4d177c6d6ec4f76eae16109336d179b2d64e4440
Veronica Rivera <veronikafranck@hotmail.com>
2023-06-09 19:43:01 +0200

    erased url

--- client/src/components/DogBreedsPage.js
+++ client/src/components/DogBreedsPage.js
@@ -3,7 +3,7 @@ import GridOfCards from "./GridOfCards";
 import Listing from "./Listing.js";
 
 
-const url = "http://localhost:5000";
+//const url = "http://localhost:5000";
 
 function DogBreedsPage() {
   const [dog_breeds, setDogBreeds] = useState([]);
@@ -15,7 +15,7 @@ function DogBreedsPage() {
 
   useEffect(() => {
     const getDogBreeds = async () => {
-      const response = await fetch(`${url}/api`);
+      const response = await fetch("https://adoptadog.herokuapp.com/");
       const data = await response.json();
       const dog_breeds = data.dog_breeds;
 
